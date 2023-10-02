1d97d249ca99048fa716c78e24082d1d0c5cb450
DooPal2000 <kkyub1313@gmail.com>
2023-10-02 23:21:36 +0900

    What I learned using npm and node.js during the Chuseok holiday

---
+++ Udemy-web-developer-75h/Modules_And_NPM_CODE/Artster/index.js
@@ -0,0 +1,22 @@
+// REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
+const figlet = require("figlet");
+const colors = require("colors");
+
+figlet('Hello Kyoung Yeob!!', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.green)
+});
+
+figlet('Have a Nice Day', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.green)
+});
+
