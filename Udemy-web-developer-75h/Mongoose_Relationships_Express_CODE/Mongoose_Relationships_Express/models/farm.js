2b71bc5f5100df462d247c8c28f768903eedec13
Kyoung Yeob <kkyub1313@gmail.com>
2024-01-30 17:57:13 +0900

    Update farm.js

--- Udemy-web-developer-75h/Mongoose_Relationships_Express_CODE/Mongoose_Relationships_Express/models/farm.js
+++ Udemy-web-developer-75h/Mongoose_Relationships_Express_CODE/Mongoose_Relationships_Express/models/farm.js
@@ -3,7 +3,7 @@ const Product = require('./product');
 const { Schema } = mongoose;
 
 // 우리가 식료품 웹사이트를 만든다고 하면, 농장에 따라 상품을 보여주는 것이 아닌
-// 모든 상품을 펼쳐 놓아야 한다. 
+// 모든 상품을 펼쳐 놓아야 한다. products, farms 둘 다 ref => 쌍방 관계를 설명하였다.
 
 const farmSchema = new Schema({
     name: {
