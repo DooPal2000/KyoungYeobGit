e4f66623a812cf2b4eaa4398855b962f03216761
DooPal2000 <kkyub1313@gmail.com>
2024-02-24 02:30:54 +0900

    Cp 51. 517~521강, Passport 구성하기

--- Udemy-web-developer-75h/YelpCamp_Atlas/app.js
+++ Udemy-web-developer-75h/YelpCamp_Atlas/app.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
-
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
 
 const campgrounds = require('./routes/camgprounds.js');
 const reviews = require('./routes/reviews.js');
@@ -54,12 +55,29 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 app.use((req,res,next)=>{
   res.locals.success = req.flash('success');
   res.locals.error  = req.flash('error');
   next();
 })
+
+app.get('/fakeUser', async(req,res) => {
+  const user = new User({email:'duq25@naver.com', username: 'yeob'})
+  const newUser = await User.register(user, 'chicken');
+  res.send(newUser);
+  
+})
+
 
 app.use('/campgrounds',campgrounds);
 app.use('/campgrounds/:id/reviews',reviews);
